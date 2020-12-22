dfa581b06442c7a3b11b44e515299fdb67a75228
seuminh <seuminh@yahoo.com>
2020-12-22 10:20:01 +0700

    design checkIn screen

--- screens/Room/checkIn.js
+++ screens/Room/checkIn.js
@@ -1,55 +1,193 @@
 import React, { Component } from "react";
 import {
-   Text,
-   StyleSheet,
-   View,
-   ScrollView,
-   ActivityIndicator,
+  Text,
+  StyleSheet,
+  View,
+  ScrollView,
+  ActivityIndicator,
+  TouchableOpacity,
 } from "react-native";
 
+import DateTimePickerModal from "react-native-modal-datetime-picker";
+import { Form, Item, Input, Label, Picker, Icon } from "native-base";
+
 export default class checkIn extends Component {
-   render() {
-      return (
-         <ScrollView style={styles.container}>
-            <Text style={styles.headerText}> Star Light Resort </Text>
-            <Text style={styles.branchText}>SK branch</Text>
-            <View style={styles.bodyContainer}>
-               <Text
-                  style={{
-                     fontSize: 17,
-                     borderBottomWidth: 1,
-                     borderBottomColor: "red",
-                  }}
-               >
-                  Check In Form
-               </Text>
+  state = {
+    checkInDateModal: false,
+    checkOutDateModal: false,
+    checkInDate: new Date(),
+    checkOutDate: new Date(),
+    paid: "paid",
+  };
+
+  toggleCheckInDateModal = () => {
+    this.setState({
+      checkInDateModal: !this.state.checkInDateModal,
+    });
+  };
+
+  toggleCheckOutDateModal = () => {
+    this.setState({
+      checkOutDateModal: !this.state.checkOutDateModal,
+    });
+  };
+
+  handleCheckInConfirm = (date) => {
+    this.setState({
+      checkInDate: new Date(date),
+      checkInDateModal: !this.state.checkInDateModal,
+    });
+  };
+
+  handleCheckOutConfirm = (date) => {
+    this.setState({
+      checkOutDate: new Date(date),
+      checkOutDateModal: !this.state.checkOutDateModal,
+    });
+  };
+
+  onValueChange = (value) => {
+    this.setState({
+      selected: value,
+    });
+  };
+
+  render() {
+    const {
+      checkInDate,
+      checkOutDate,
+      checkInDateModal,
+      checkOutDateModal,
+      paid,
+    } = this.state;
+
+    return (
+      <ScrollView style={styles.container}>
+        <Text style={styles.headerText}> Star Light Resort </Text>
+        <Text style={styles.branchText}>SK branch</Text>
+        <View style={styles.bodyContainer}>
+          <Text
+            style={{
+              fontSize: 20,
+              //   borderBottomWidth: 1,
+              //   borderBottomColor: "red",
+              textAlign: "center",
+            }}
+          >
+            Check In Form
+          </Text>
+          <Form>
+            <Item floatingLabel>
+              <Label>Name</Label>
+              <Input />
+            </Item>
+            <Item floatingLabel last>
+              <Label>ID Number</Label>
+              <Input />
+            </Item>
+            {/* Check In */}
+            <View style={styles.checkInContainer}>
+              <Text style={styles.biggerText}>Check in :</Text>
+              <TouchableOpacity onPress={this.toggleCheckInDateModal}>
+                <Text style={[styles.biggerText, { marginLeft: 32 }]}>
+                  {checkInDate.toLocaleDateString()}
+                </Text>
+              </TouchableOpacity>
             </View>
-         </ScrollView>
-      );
-   }
+            {/* Check Out */}
+            <View style={styles.checkOutContainer}>
+              <Text style={styles.biggerText}>Check out :</Text>
+              <TouchableOpacity onPress={this.toggleCheckOutDateModal}>
+                <Text style={[styles.biggerText, { marginLeft: 20 }]}>
+                  {checkOutDate.toLocaleDateString()}
+                </Text>
+              </TouchableOpacity>
+            </View>
+            {/* Payment */}
+            <View style={styles.paymentContainer}>
+              <Text style={styles.biggerText}>Payment :</Text>
+              <Picker
+                mode="dropdown"
+                iosIcon={<Icon name="arrow-down" />}
+                style={{
+                  marginLeft: 17,
+                  marginTop: -11,
+                }}
+                selectedValue={paid}
+                onValueChange={this.onValueChange}
+              >
+                <Picker.Item label="Paid" value="paid" />
+                <Picker.Item label="Not yet pay" value="notPaid" />
+              </Picker>
+            </View>
+            {/* Room */}
+            <View>
+              <Text>Room</Text>
+            </View>
+          </Form>
+        </View>
+
+        {/* Modal */}
+        <DateTimePickerModal
+          isVisible={checkInDateModal}
+          mode="date"
+          onConfirm={this.handleCheckInConfirm}
+          onCancel={this.toggleCheckInDateModal}
+          date={new Date()}
+          isDarkModeEnabled={false}
+        />
+
+        <DateTimePickerModal
+          isVisible={checkOutDateModal}
+          mode="date"
+          onConfirm={this.handleCheckOutConfirm}
+          onCancel={this.toggleCheckOutDateModal}
+          date={new Date()}
+          isDarkModeEnabled={false}
+        />
+      </ScrollView>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-   container: {
-      flex: 1,
-      paddingHorizontal: 10,
-      paddingTop: 10,
-   },
-   headerText: {
-      fontWeight: "500",
-      fontSize: 30,
-   },
-   branchText: {
-      fontSize: 14,
-      color: "#6A0E00",
-      marginVertical: 8,
-      paddingHorizontal: 7,
-   },
-   bodyContainer: {
-      paddingHorizontal: 7,
-      paddingTop: 8,
-      marginVertical: 5,
-      flex: 1,
-      alignItems: "center",
-   },
+  container: {
+    flex: 1,
+    paddingHorizontal: 10,
+    paddingTop: 10,
+  },
+  biggerText: {
+    fontSize: 17,
+  },
+  headerText: {
+    fontWeight: "500",
+    fontSize: 30,
+  },
+  branchText: {
+    fontSize: 14,
+    color: "#6A0E00",
+    marginVertical: 8,
+    paddingHorizontal: 7,
+  },
+  bodyContainer: {
+    paddingHorizontal: 7,
+    paddingTop: 8,
+    marginVertical: 5,
+    flex: 1,
+  },
+  checkInContainer: {
+    flexDirection: "row",
+    marginTop: 15,
+    paddingLeft: 14,
+  },
+  checkOutContainer: {
+    flexDirection: "row",
+    paddingLeft: 14,
+    marginTop: 13,
+  },
+  paymentContainer: {
+    paddingLeft: 14,
+    marginTop: 13,
+    flexDirection: "row",
+  },
 });
--- screens/Room/index.js
+++ screens/Room/index.js
@@ -1,150 +1,158 @@
 import React, { Component } from "react";
 import {
-   Text,
-   StyleSheet,
-   View,
-   TouchableOpacity,
-   ScrollView,
-   ActivityIndicator,
+  Text,
+  StyleSheet,
+  View,
+  TouchableOpacity,
+  ScrollView,
+  ActivityIndicator,
 } from "react-native";
 import DateTimePickerModal from "react-native-modal-datetime-picker";
 
 import Card from "../../components/RoomCard";
 
 export default class index extends Component {
-   constructor(props) {
-      super(props);
-      this.state = {
-         dateModal: false,
-         date: new Date(),
-         loading: true,
-      };
-   }
+  constructor(props) {
+    super(props);
+    this.state = {
+      dateModal: false,
+      date: new Date(),
+      loading: true,
+    };
+  }
 
-   toggleDateModal = () => {
+  toggleDateModal = () => {
+    this.setState({
+      dateModal: !this.state.dateModal,
+    });
+  };
+
+  handleConfirm = (date) => {
+    this.setState(
+      {
+        date: new Date(date),
+        dateModal: !this.state.dateModal,
+        loading: true,
+      },
+      () => {
+        this.fetchRoom();
+      }
+    );
+  };
+
+  fetchRoom = () => {
+    setTimeout(() => {
       this.setState({
-         dateModal: !this.state.dateModal,
+        loading: false,
       });
-   };
+    }, 500);
+  };
 
-   handleConfirm = (date) => {
+  goCheckIn = () => {
+    this.props.navigation.navigate("CheckIn");
+  };
+
+  componentDidMount() {
+    setTimeout(() => {
       this.setState({
-         date: new Date(date),
-         dateModal: !this.state.dateModal,
+        loading: false,
       });
-   };
+    }, 100);
+  }
 
-   goCheckIn = () => {
-      this.props.navigation.navigate("CheckIn");
-   };
+  render() {
+    const { dateModal, date, loading } = this.state;
+    return (
+      <ScrollView style={styles.container}>
+        <Text style={styles.headerText}> Star Light Resort </Text>
+        <View style={styles.subHeaderContainer}>
+          <Text style={styles.branchText}>SK branch</Text>
+          <TouchableOpacity onPress={this.toggleDateModal}>
+            <Text>Select Date</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.selectDateButton}>
+          <Text>{date.toLocaleDateString()}</Text>
+        </View>
 
-   componentDidMount() {
-      setTimeout(() => {
-         this.setState({
-            loading: false,
-         });
-      }, 100);
-   }
-
-   render() {
-      const { dateModal, date, loading } = this.state;
-      return (
-         <ScrollView style={styles.container}>
-            <Text style={styles.headerText}> Star Light Resort </Text>
-            <View style={styles.subHeaderContainer}>
-               <Text style={styles.branchText}>SK branch</Text>
-               <TouchableOpacity onPress={this.toggleDateModal}>
-                  <Text>Select Date</Text>
-               </TouchableOpacity>
+        <View style={styles.bodyContainer}>
+          {!loading && (
+            <View style={styles.cardContainer}>
+              <Card status="busy"></Card>
+              <Card status="available"></Card>
+              <Card status="available"></Card>
+              <Card status="reserved"></Card>
+              <Card status="available"></Card>
+              <Card status="available"></Card>
             </View>
-            <View style={styles.selectDateButton}>
-               <Text>{date.toLocaleDateString()}</Text>
-            </View>
+          )}
 
-            <View style={styles.bodyContainer}>
-               {!loading && (
-                  <View style={styles.cardContainer}>
-                     <Card status="busy"></Card>
-                     <Card status="available"></Card>
-                     <Card status="available"></Card>
-                     <Card status="reserved"></Card>
-                     <Card status="available"></Card>
-                     <Card status="available"></Card>
-                  </View>
-               )}
+          {loading && (
+            <ActivityIndicator color="red" size="large"></ActivityIndicator>
+          )}
 
-               {loading && (
-                  <ActivityIndicator
-                     color="red"
-                     size="large"
-                  ></ActivityIndicator>
-               )}
+          <TouchableOpacity style={styles.btnCheckIn} onPress={this.goCheckIn}>
+            <Text style={styles.btnCheckInText}>Check In</Text>
+          </TouchableOpacity>
+        </View>
 
-               <TouchableOpacity
-                  style={styles.btnCheckIn}
-                  onPress={this.goCheckIn}
-               >
-                  <Text style={styles.btnCheckInText}>Check In</Text>
-               </TouchableOpacity>
-            </View>
-
-            {/* Modal */}
-            <DateTimePickerModal
-               isVisible={dateModal}
-               mode="date"
-               onConfirm={this.handleConfirm}
-               onCancel={this.toggleDateModal}
-               date={new Date()}
-               isDarkModeEnabled={false}
-            />
-         </ScrollView>
-      );
-   }
+        {/* Modal */}
+        <DateTimePickerModal
+          isVisible={dateModal}
+          mode="date"
+          onConfirm={this.handleConfirm}
+          onCancel={this.toggleDateModal}
+          date={new Date()}
+          isDarkModeEnabled={false}
+        />
+      </ScrollView>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-   container: {
-      flex: 1,
-      paddingHorizontal: 10,
-      paddingTop: 10,
-   },
-   headerText: {
-      fontWeight: "500",
-      fontSize: 30,
-   },
-   subHeaderContainer: {
-      flexDirection: "row",
-      justifyContent: "space-between",
-      marginVertical: 8,
-      paddingHorizontal: 7,
-   },
-   branchText: {
-      fontSize: 14,
-      color: "#6A0E00",
-   },
-   selectDateButton: {
-      paddingHorizontal: 7,
-      alignSelf: "flex-end",
-   },
-   bodyContainer: {
-      flex: 1,
-      justifyContent: "center",
-      flexDirection: "column",
-      alignItems: "center",
-   },
-   cardContainer: {
-      flexDirection: "row",
-      flexWrap: "wrap",
-      justifyContent: "center",
-      marginTop: 20,
-   },
-   btnCheckIn: {
-      marginTop: 40,
-      padding: 10,
-      paddingHorizontal: 60,
-      backgroundColor: "darkslateblue",
-   },
-   btnCheckInText: {
-      color: "#fff",
-   },
+  container: {
+    flex: 1,
+    paddingHorizontal: 10,
+    paddingTop: 10,
+  },
+  headerText: {
+    fontWeight: "500",
+    fontSize: 30,
+  },
+  subHeaderContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginVertical: 8,
+    paddingHorizontal: 7,
+  },
+  branchText: {
+    fontSize: 14,
+    color: "#6A0E00",
+  },
+  selectDateButton: {
+    paddingHorizontal: 7,
+    alignSelf: "flex-end",
+  },
+  bodyContainer: {
+    flex: 1,
+    justifyContent: "center",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  cardContainer: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "center",
+    marginTop: 20,
+  },
+  btnCheckIn: {
+    marginTop: 40,
+    padding: 10,
+    paddingHorizontal: 60,
+    backgroundColor: "darkslateblue",
+  },
+  btnCheckInText: {
+    color: "#fff",
+  },
 });
