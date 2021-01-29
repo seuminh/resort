e075e162a12ecc76a5a876862f8ec9ae7de1d245
seuminh <seuminh@yahoo.com>
2021-01-29 11:46:30 +0700

    update income screen option

--- screens/Admin/Dashboard/income.js
+++ screens/Admin/Dashboard/income.js
@@ -15,13 +15,14 @@ import {
   DefaultTheme,
   Dialog,
 } from "react-native-paper";
-import { AntDesign } from "@expo/vector-icons";
+import { AntDesign, Feather } from "@expo/vector-icons";
 
 export default class income extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      modalSort: false,
+      modalSort: true,
+      sort: "today",
     };
   }
 
@@ -31,8 +32,14 @@ export default class income extends Component {
     });
   };
 
+  onSort = (value) => {
+    this.setState({
+      sort: value,
+    });
+  };
+
   render() {
-    const { modalSort } = this.state;
+    const { modalSort, sort } = this.state;
     const theme = {
       ...DefaultTheme,
     };
@@ -76,8 +83,103 @@ export default class income extends Component {
                 Sort by Date
               </Text>
               <View>
-                <Text>hi</Text>
-                <Text>Hello</Text>
+                <TouchableOpacity
+                  style={styles.optionContainer}
+                  onPress={() => this.onSort("today")}
+                >
+                  <View>
+                    <Text
+                      style={[
+                        styles.option,
+                        { color: sort === "today" ? "green" : "#000" },
+                      ]}
+                    >
+                      Today
+                    </Text>
+                    <Text style={{ fontSize: 12, color: "#888" }}>
+                      {new Date().toLocaleDateString()}
+                    </Text>
+                  </View>
+                  {sort === "today" && (
+                    <Feather name="check" size={24} color="green" />
+                  )}
+                </TouchableOpacity>
+                <TouchableOpacity
+                  style={styles.optionContainer}
+                  onPress={() => this.onSort("yesterday")}
+                >
+                  <View>
+                    <Text
+                      style={[
+                        styles.option,
+                        { color: sort === "yesterday" ? "green" : "#000" },
+                      ]}
+                    >
+                      Yesterday
+                    </Text>
+                    <Text style={{ fontSize: 12, color: "#888" }}>Hello</Text>
+                  </View>
+                  {sort === "yesterday" && (
+                    <Feather name="check" size={24} color="green" />
+                  )}
+                </TouchableOpacity>
+                <TouchableOpacity
+                  style={styles.optionContainer}
+                  onPress={() => this.onSort("lastWeek")}
+                >
+                  <View>
+                    <Text
+                      style={[
+                        styles.option,
+                        { color: sort === "lastWeek" ? "green" : "#000" },
+                      ]}
+                    >
+                      Last week
+                    </Text>
+                    <Text style={{ fontSize: 12, color: "#888" }}>Hello</Text>
+                  </View>
+                  {sort === "lastWeek" && (
+                    <Feather name="check" size={24} color="green" />
+                  )}
+                </TouchableOpacity>
+                <TouchableOpacity
+                  style={styles.optionContainer}
+                  onPress={() => this.onSort("lastMonth")}
+                >
+                  <View>
+                    <Text
+                      style={[
+                        styles.option,
+                        { color: sort === "lastMonth" ? "green" : "#000" },
+                      ]}
+                    >
+                      Last month
+                    </Text>
+                    <Text style={{ fontSize: 12, color: "#888" }}>Hello</Text>
+                  </View>
+                  {sort === "lastMonth" && (
+                    <Feather name="check" size={24} color="green" />
+                  )}
+                </TouchableOpacity>
+                <TouchableOpacity
+                  style={styles.optionContainer}
+                  onPress={() => this.onSort("thisMonth")}
+                >
+                  <View>
+                    <Text
+                      style={[
+                        styles.option,
+                        { color: sort === "thisMonth" ? "green" : "#000" },
+                      ]}
+                    >
+                      This month
+                    </Text>
+                    <Text style={{ fontSize: 12, color: "#888" }}>Hello</Text>
+                  </View>
+                  {sort === "thisMonth" && (
+                    <Feather name="check" size={24} color="green" />
+                  )}
+                </TouchableOpacity>
               </View>
             </View>
           </Modal>
@@ -112,6 +214,15 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
     marginTop: -100,
     borderRadius: 20,
-    height: 250,
+    height: 350,
+  },
+  optionContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingHorizontal: 30,
+    marginBottom: 10,
+  },
+  option: {
+    fontSize: 15,
   },
 });
